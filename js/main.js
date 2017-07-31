54fd62dcd242754b8c4f603bf77f0f38906e85b2
Mario Cannistrà <Metsuryu@users.noreply.github.com>
2017-07-31 22:03:14 +0200

    Add more planets

--- js/main.js
+++ js/main.js
@@ -9,32 +9,31 @@
         function preload(){
 
             game.load.image("sun", "img/Sun.jpg");
+            game.load.image("shadow", "img/Shadow.png");
+
+            game.load.image("mercury", "img/Mercury.jpg");
+            game.load.image("venus", "img/Venus.jpg");
+
             game.load.image("earth", "img/Earth/Earth.jpg");
             game.load.image("moon", "img/Earth/Moon.jpg");
-            game.load.image("shadow", "img/Shadow.png");
+
             game.load.image("mars", "img/Mars/Mars.jpg");
             game.load.image("deimos", "img/Mars/Deimos.jpg");
             game.load.image("phobos", "img/Mars/Phobos.jpg");
 
+            game.load.image("jupiter", "img/Jupiter/Jupiter.jpg");
+
             game.load.image("ship", "img/Spaceship.png");
         }
 
-        var sun;
-        
-        var earth;
-        var moon;
-        
-        var mars;
-        var deimos;
-        var phobos;
-
+        var sun, mercury, venus, earth, moon, mars, deimos, phobos;
+        //Completely arbitrary rotation speed
         var rotationSpeed = 0.021;
-        var revolutionSpeed = 1; //TODO: Implemented in orbit, make a GUI to change it.
+        var revolutionSpeed = 1; //TODO: Multiplier. Implemented in orbit(), maybe make a GUI to change it.
         var ship;
 
         var angle = 3 * Math.PI / 180;
 
-        //TODO A: Finish planet constructor
         function celestialBody(sprite, spriteX, spriteY, anchorX, anchorY, revolveAround, revolutionRadius) {
             this.sprite = game.add.sprite(spriteX, spriteY, sprite);
             //Center of the sprite
@@ -57,106 +56,151 @@
             this.shadow.scale.setTo(scaleX,scaleY);
         }
 
-
         function create(){
             game.physics.startSystem(Phaser.Physics.ARCADE);
+            //TODO: Maybe add a starry space background instead of pure black
             //game.stage.backgroundColor = "#4488AA";
             //TODO: Adjust bounds
             game.world.setBounds(0, 0, 1280, 1024);
             
-            var planetOffset = 120;
+            var planetRevolutionRadius = 80;
             var moonOffset = 25;
+            var nthMoonOffset = 1.55;
+            var centerAnchorX = 0.5;
+            var centerAnchorY = 0.5;
+            var baseScale = 1;
             var worldCenterX = game.world.centerX;
             var worldCenterY = game.world.centerY;
 
-            //TODO: Maybe give an GFX to the sun.
             sun = new celestialBody(
                 "sun",
                 worldCenterX, worldCenterY,
-                0.5, 0.5
+                centerAnchorX, centerAnchorY
                 //The sun doesn't need other parameters
             );
 
             var sunX = sun.sprite.position.x;
             var sunY = sun.sprite.position.y;
             var sunCenter = {"x": sunX, "y": sunY };
-            var earthStartX = sunX + planetOffset;
+
+            mercury = new celestialBody(
+                "mercury",
+                worldCenterX, worldCenterY,
+                centerAnchorX, centerAnchorY,
+                sunCenter,
+                planetRevolutionRadius
+            );
+            var mercuryShadowAlpha = 0.5;
+            mercury.addShadow(
+                worldCenterX, worldCenterY,
+                mercuryShadowAlpha,
+                baseScale, baseScale
+            );
+
+            venus = new celestialBody(
+                "venus",
+                worldCenterX, worldCenterY,
+                centerAnchorX, centerAnchorY,
+                sunCenter,
+                planetRevolutionRadius*2
+            );
+            var venusShadowAlpha = 0.5;
+            venus.addShadow(
+                worldCenterX, worldCenterY,
+                venusShadowAlpha,
+                baseScale, baseScale
+            );
 
             earth = new celestialBody(
                 "earth",
-                earthStartX, worldCenterY,
-                0.5, 0.5,
+                worldCenterX, worldCenterY,
+                centerAnchorX, centerAnchorY,
                 sunCenter,
-                80
+                planetRevolutionRadius*3
             );
 
             var earthCenter = {"x": earth.sprite.position.x, "y": earth.sprite.position.y };
 
             moon = new celestialBody(
                 "moon",
-                earth.sprite.position.x + moonOffset, worldCenterY,
-                0.5, 0.5,
+                earth.sprite.position.x, worldCenterY,
+                centerAnchorX, centerAnchorY,
                 earthCenter,
-                25
+                moonOffset
             );
 
+            var moonShadowAlpha = 0.3;
+            var moonShadowScaleY = 0.2;
             moon.addShadow(
-                earth.sprite.position.x + moonOffset, worldCenterY,
-                0.3,
-                1, 0.2
+                earth.sprite.position.x, worldCenterY,
+                moonShadowAlpha,
+                baseScale, moonShadowScaleY
             );
 
+            var earthShadowAlpha = 0.4;
             //Earth's shadow is after the moon, since it has to cover it.
             earth.addShadow(
-                earthStartX, worldCenterY,
-                0.4,
-                1, 1
+                worldCenterX, worldCenterY,
+                earthShadowAlpha,
+                baseScale, baseScale
             );
 
-            var marsStartX = earthStartX+planetOffset;
             mars = new celestialBody(
                 "mars",
-                marsStartX, worldCenterY,
-                0.5, 0.5,
+                worldCenterX, worldCenterY,
+                centerAnchorX, centerAnchorY,
                 sunCenter,
-                170
+                planetRevolutionRadius*4
             );
             var marsCenter = {"x": mars.sprite.position.x, "y": mars.sprite.position.y };
             deimos = new celestialBody(
                 "deimos",
-                mars.sprite.position.x + moonOffset, worldCenterY,
-                0.5, 0.5,
+                mars.sprite.position.x, worldCenterY,
+                centerAnchorX, centerAnchorY,
                 marsCenter,
-                25
+                moonOffset
             );
+            var deimosShadowAlpha = 0.15;
+            var deimosShadowScaleY = 0.2;
             deimos.addShadow(
-                mars.sprite.position.x + moonOffset, worldCenterY,
-                0.15,
-                1, 0.2
+                mars.sprite.position.x, worldCenterY,
+                deimosShadowAlpha,
+                baseScale, deimosShadowScaleY
             );
-
             phobos = new celestialBody(
                 "phobos",
-                mars.sprite.position.x + moonOffset, worldCenterY,
-                0.5, 0.5,
+                mars.sprite.position.x, worldCenterY,
+                centerAnchorX, centerAnchorY,
                 marsCenter,
-                40
+                moonOffset*nthMoonOffset
             );
-            deimos.addShadow(
-                mars.sprite.position.x + moonOffset, worldCenterY,
-                0.15,
-                1, 0.2
+            var phobosShadowAlpha = 0.15;
+            var phobosShadowScaleY = 0.2;
+            phobos.addShadow(
+                mars.sprite.position.x, worldCenterY,
+                phobosShadowAlpha,
+                baseScale, phobosShadowScaleY
+            );
+            var marsShadowAlpha = 0.2;
+            var marsShadowScaleX = 0.8;
+            mars.addShadow(
+                worldCenterX, worldCenterY,
+                marsShadowAlpha,
+                marsShadowScaleX, baseScale
             );
 
-            mars.addShadow(
-                marsStartX, worldCenterY,
-                0.2,
-                0.8, 1
+
+            jupiter = new celestialBody(
+                "jupiter",
+                worldCenterX, worldCenterY,
+                centerAnchorX, centerAnchorY,
+                sunCenter,
+                planetRevolutionRadius*5
             );
 
             //TODO: Maybe remove, or make a transparent sprite for camera movement
             ship = game.add.sprite(worldCenterX-200, worldCenterY, "ship");
-            ship.anchor.setTo(0.5, 0.5);
+            ship.anchor.setTo(centerAnchorX, centerAnchorY);
             game.physics.arcade.enable(ship);
             ship.body.collideWorldBounds = true;
             cursors = game.input.keyboard.createCursorKeys();
@@ -186,9 +230,13 @@
             }
         }
 
-        function orbit(planet, orbitSpeed){
+        function orbit(planet, orbitSpeed, orbitAround){
 
-            //TODO: If moon, make it follow the planet in here.
+            //If it's a moon, make it follow its planet (orbitAround).
+            if (orbitAround) {
+                planet.revolveAround = {"x": orbitAround.sprite.position.x, "y": orbitAround.sprite.position.y };
+            };
+
             //Update shadow
             if (planet.shadow) {
                 //Face away from sun
@@ -197,7 +245,6 @@
                 planet.shadow.x = planet.sprite.position.x;
                 planet.shadow.y = planet.sprite.position.y;
             };
-
 
             orbitSpeed = orbitSpeed*revolutionSpeed;
             // increase the angle of rotation at each frame
@@ -211,23 +258,24 @@
         }
 
         function update(){
+            mercury.sprite.rotation += rotationSpeed;
+            venus.sprite.rotation += rotationSpeed;
             earth.sprite.rotation += rotationSpeed;
-            
+            mars.sprite.rotation += rotationSpeed;
 
-            orbit(earth, 0.1);
+            //TODO: Calculate speeds of new planets based on year lenght.
+            orbit(mercury, -0.21);
+            orbit(venus, -0.11);
 
-            //Orbit around earth, update the position each frame since earth is moving.
-            //TODO: Adjust moon's speed.
-            moon.revolveAround = {"x": earth.sprite.position.x, "y": earth.sprite.position.y };
-            orbit(moon, 0.3);
+            orbit(earth, -0.1);
+            orbit(moon, 0.3, earth);
 
             //Mars's year is about twice as long as Earth's year.
-            orbit(mars, 0.053);
+            orbit(mars, -0.053);
+            orbit(deimos, 0.42, mars);
+            orbit(phobos, 0.14, mars);
 
-            deimos.revolveAround = {"x": mars.sprite.position.x, "y": mars.sprite.position.y };
-            orbit(deimos, 0.42);
-            phobos.revolveAround = {"x": mars.sprite.position.x, "y": mars.sprite.position.y };
-            orbit(phobos, 0.14);
+            orbit(jupiter, -0.023);
 
             moveShip();
         }
